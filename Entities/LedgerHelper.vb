cc646769dc4ccccfc1f0c0cc81ca47f41b94cd38
yadavpankaj88 <yadavpankaj88@gmail.com>
2016-01-16 16:04:52 +0530

    corrected issue with date time in voucher confirm

--- Entities/LedgerHelper.vb
+++ Entities/LedgerHelper.vb
@@ -15,21 +15,40 @@ Public Class LedgerHelper
     End Sub
     Public Sub AddLedgerDetail(ByVal VoucherLinkNo As String)
 
-        Dim query As String = String.Empty
-        query = "SELECT VH_vch_Dt,VH_lnk_Dt FROM " + InstitutionMasterData.XInstType + "_Voucher_Header where VH_Lnk_No='" + VoucherLinkNo + "'"
-        Dim dt As DataTable = New DataTable()
-        Dim dHelper As DataHelper = New DataHelper()
-        dt = dHelper.ExecuteQuery(query, CommandType.Text, Nothing)
-
-        Dim VH_vch_Dt As String = String.Empty
-        Dim VH_lnk_Dt As String = String.Empty
-        If (dt.Rows.Count > 0) Then
-            VH_vch_Dt = dt.Rows(0)(0).ToString()
-            VH_lnk_Dt = dt.Rows(0)(1).ToString()
-        End If
-
-        Dim saveQuery = "INSERT INTO " + InstitutionMasterData.XInstType + "_Ledger([Lgr_vch_dt],[Lgr_lnk_dt],[Lgr_Fin_Yr],[Lgr_Seq_No],[Lgr_Inst_Cd],[Lgr_Inst_Typ],[Lgr_Brn_Cd ],[Lgr_Lnk_No],[Lgr_Dbk_Cd],[Lgr_Trn_Typ],[Lgr_Vch_No],[Lgr_Vch_Ref_No],[Lgr_Narr],[Lgr_Ref_No],[Lgr_Ref_Dt],[Lgr_Acc_Cd],[Lgr_Amt],[Lgr_Cr_Dr],[Lgr_ABS_Amt],[Lgr_Ent_By],[Lgr_Ent_Dt],[Lgr_Upd_By],[Lgr_Upd_Dt],[Lgr_Conf_By],[Lgr_Conf_Dt])" + _
-           " SELECT '" + VH_vch_Dt + "','" + VH_lnk_Dt + "',VD_Fin_Yr,VD_Seq_No,VD_Inst_Cd,VD_Inst_Typ,VD_Brn_Cd,VD_Lnk_No,VD_Dbk_Cd,VD_Trn_Typ ,VD_Vch_No ,VD_Vch_Ref_No ,VD_Narr,VD_Ref_No,VD_Ref_Dt,VD_Acc_Cd,VD_Amt,VD_Cr_Dr,VD_ABS_Amt,'" + InstitutionMasterData.XUsrId + "',GETDATE(),'" + InstitutionMasterData.XUsrId + "',GETDATE(),'" + InstitutionMasterData.XUsrId + "',GETDATE() from dbo." + InstitutionMasterData.XInstType + "_Voucher_Detail where VD_Lnk_No='" + VoucherLinkNo + "'"
+        Dim saveQuery = "INSERT INTO " + InstitutionMasterData.XInstType + "_Ledger(" & _
+                            "[Lgr_vch_dt]" & _
+                            ",[Lgr_lnk_dt]" & _
+                            ",[Lgr_Fin_Yr]" & _
+                            ",[Lgr_Seq_No]" & _
+                            ",[Lgr_Inst_Cd]" & _
+                            ",[Lgr_Inst_Typ]" & _
+                            ",[Lgr_Brn_Cd ]" & _
+                            ",[Lgr_Lnk_No]" & _
+                            ",[Lgr_Dbk_Cd]" & _
+                            ",[Lgr_Trn_Typ]" & _
+                            ",[Lgr_Vch_No]" & _
+                            ",[Lgr_Vch_Ref_No]" & _
+                            ",[Lgr_Narr]" & _
+                            ",[Lgr_Ref_No]" & _
+                            ",[Lgr_Ref_Dt]" & _
+                            ",[Lgr_Acc_Cd]" & _
+                            ",[Lgr_Amt]" & _
+                            ",[Lgr_Cr_Dr]" & _
+                            ",[Lgr_ABS_Amt]" & _
+                            ",[Lgr_Ent_By]" & _
+                            ",[Lgr_Ent_Dt]" & _
+                            ",[Lgr_Upd_By]" & _
+                            ",[Lgr_Upd_Dt]" & _
+                            ",[Lgr_Conf_By]" & _
+                            ",[Lgr_Conf_Dt])" + _
+                            " SELECT " + _
+                            "vh.VH_Vch_Dt" + _
+                            ",vh.VH_Lnk_Dt" + _
+                            ",VD_Fin_Yr,VD_Seq_No,VD_Inst_Cd,VD_Inst_Typ,VD_Brn_Cd,VD_Lnk_No,VD_Dbk_Cd,VD_Trn_Typ ,VD_Vch_No ,VD_Vch_Ref_No ,VD_Narr,VD_Ref_No,VD_Ref_Dt,VD_Acc_Cd,VD_Amt,VD_Cr_Dr,VD_ABS_Amt,'11111',GETDATE(),'11111',GETDATE(),'11111',GETDATE() " + _
+                            "from dbo." + InstitutionMasterData.XInstType + "_Voucher_Detail VD " + _
+                            "INNER JOIN " + InstitutionMasterData.XInstType + "_Voucher_Header VH " + _
+                            "ON VD.VD_Lnk_No=vh.VH_Lnk_No " + _
+                            "where VD_Lnk_No='" + VoucherLinkNo + "'"
 
         dataHelp.ExecuteNonQuery(saveQuery, CommandType.Text)
     End Sub
